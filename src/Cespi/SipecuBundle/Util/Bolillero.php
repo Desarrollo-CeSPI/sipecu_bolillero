2b9890ca394c218141bb9f291eba7ae0019e7ede
Luciano Coggiola <lucianoc@cespi.unlp.edu.ar>
2015-10-26 16:00:15 -0300

    Revert "Cambiado el tipo de variable por diferencias en arquitecturas distintas y el tamaño de los enteros en 32 y 64 bits: Microsegundos en float: "segundos.microsegundos", para arquitecturas de 32 y 64 bits, ya que el tamaño del entero en microsegundos con 6 decimales de precisión no entra en 32bits. Ahora es el mismo valor independientemente de la arquitectura en la que se ejecuta."
    This reverts commit c19c4df88905f27612d94d1f205ec7682eb813a4.

--- src/Cespi/SipecuBundle/Util/Bolillero.php
+++ src/Cespi/SipecuBundle/Util/Bolillero.php
@@ -3,9 +3,16 @@
 namespace Cespi\SipecuBundle\Util;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sipecu Bolillero - Módulo que implementa el sorteo para la asignación de vacantes a los colegios de la UNLP.
+ * Copyright (C) 2015 CeSPI - UNLP <sistemasacademicos@unlp.edu.ar>
+ * 
+ * Este archivo es parte de SIPECU.
+ *
+ * Bolillero.php es software libre:  puede redistribuirlo y/o modificarlo 
+ * bajo los términos de la GNU General Public License v2.0 <http://www.gnu.org/licenses/gpl-2.0.html>.
+ *
+ * Versión: 1.0
+ * 
  */
 
 /**
@@ -20,6 +27,10 @@ class Bolillero
     private $sorteado = false;
     private $numerosSorteados = array();
     private $semilla = null;
+<<<<<<< HEAD
+    private $strMicrotime = null;
+=======
+>>>>>>> 1a44f39dc6d302f281fb385159ee8d8b928bede8
 
     public function __construct($mayorNumero = null)
     {
@@ -52,6 +63,10 @@ class Bolillero
     private function generarSemilla()
     {
         list($microsegundos, $segundos) = explode(' ', microtime()); //Flotante: "segundos microsegundos" (6 decimales de precisión)
+<<<<<<< HEAD
+        $this->strMicrotime = $segundos .';'. $microsegundos;
+=======
+>>>>>>> 1a44f39dc6d302f281fb385159ee8d8b928bede8
         $segundos = (int) ($segundos * 1000000); // 6 decimales de microsegundos
         $microsegundos = (int) ($microsegundos * 1000000);
         $this->semilla = $segundos + $microsegundos; // microsegundos hasta la fecha (entero)
@@ -62,7 +77,15 @@ class Bolillero
     {
         return $this->semilla;
     }
+<<<<<<< HEAD
+
+    public function getStrMicrotime()
+    {
+        return $this->strMicrotime;
+    }
+=======
     
+>>>>>>> 1a44f39dc6d302f281fb385159ee8d8b928bede8
    
     public function sortear()
     {
